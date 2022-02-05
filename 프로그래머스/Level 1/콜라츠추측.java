5d8107dc37725f4db607f1e1c89c843193b8beaf
hyeinn665 <96910427+hyeinn665@users.noreply.github.com>
2022-02-05 23:14:13 +0900

    programmers

---
+++ 프로그래머스/Level 1/콜라츠추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long a = num;
+        while(a!=1){
+            if(a%2 == 0){
+                a /=2;
+                answer++;
+            }else if(a%2!=0){
+                a = (a*3)+1;
+                answer++;
+            }
+            if(answer>500){
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
