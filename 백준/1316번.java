9bd9df7492488b0447c5d0e7173f0b48ce7c2227
hyeinn665 <96910427+hyeinn665@users.noreply.github.com>
2022-02-09 10:53:43 +0900

    그룹 단어 체커

---
+++ 백준/1316번.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int result = 0;
+		for(int i=0; i<n; i++) {
+			String str = sc.next();
+			boolean check[] = new boolean[26];
+			boolean tmp = true;
+			for(int j=0; j<str.length(); j++) {
+				int index = str.charAt(j)-'a';
+				if(check[index]) {
+					if(str.charAt(j)!=str.charAt(j-1)) {
+						tmp = false;
+						break;
+					}
+				}else {
+					check[index]=true;
+				}
+			}
+			if(tmp) result++;
+		}
+		System.out.println(result);
+	}
+}
